d2d74fc6b3fa271a12aedc84ef0b086539ff437e
lisaula <luisca_elfather@hotmail.com>
2016-12-13 16:00:27 -0600

    given some style

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import RutasView from './components/rutas/rutasView';
 import RouteDirection from './components/rutas/routeDirection';
 import RoutePlate from './components/rutas/routePlate';
 import ScanningView from './components/scanner/scanningView';
+import theme from './themes/base-theme';
 import {
   Router,
   Scene,
@@ -19,7 +20,7 @@ class App extends React.Component {
     return (
       <Router>
         <Scene key='root' style = {{paddingTop: Platform.OS ==='ios'? 64: 54}}>
-          <Scene key='home' component={Home} title='Home' />
+          <Scene key='home' theme={theme} component={Home} title='Home' />
           <Scene key='rutasView' component={RutasView} title='Rutas' />
           <Scene key='routeDirection' component={RouteDirection} title='Entra o Salida de Ruta' />
           <Scene key='routePlate' component={RoutePlate} title='Ingreso de placa' />
--- src/components/home.js
+++ src/components/home.js
@@ -4,15 +4,20 @@ import{
 	View,
 	Text,
 	StyleSheet,
-	TouchableOpacity
+	TouchableOpacity,
+	Dimensions,
+	Button
 } from 'react-native';
 import {
 	Actions,
 } from 'react-native-router-flux';
+
+const deviceHeight = Dimensions.get('window').height;
+
 class Home extends React.Component {
 	render(){
 		return (
-			<View>
+			<View theme={this.props.theme} style={styles.container}>
 				<Text style ={styles.title}>
 					Hello Home
 				</Text>
@@ -20,19 +25,52 @@ class Home extends React.Component {
 							Actions.rutasView();
 					}}
 					>
-					<Text>
-						Rutas
+					<Text style={styles.btn}>
+						Cargar Rutas
 					</Text>
 				</TouchableOpacity>
+			<TouchableOpacity style={styles.btn}>
+				<Text>	Sync </Text>
+			</TouchableOpacity>
 			</View>
 		);
 	}
 }
-var styles = StyleSheet.create({
+
+let styles = StyleSheet.create({
 	title:{
 		marginTop:20,
 		marginLeft:20,
 		fontSize:20,
-	}
+	},
+	container: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: '#FBFAFA',
+  },
+  shadow: {
+    flex: 1,
+    width: null,
+    height: null,
+  },
+  bg: {
+    flex: 1,
+    marginTop: deviceHeight / 1.75,
+    paddingTop: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 30,
+    bottom: 0,
+  },
+  input: {
+    marginBottom: 20,
+  },
+  btn: {
+    marginTop: 20,
+    alignSelf: 'center',
+  },
 });
 export default Home;
